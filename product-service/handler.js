430583ad99162dc1a45916fa062bf86318c35d9d
Vitaly Bury <vitalybury@coherentsolutions.com>
2022-06-13 02:10:34 +0300

    fix: added headers for correct response

--- product-service/handler.js
+++ product-service/handler.js
@@ -4,6 +4,10 @@ const products = require('./products');
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(products)
   };
 };
