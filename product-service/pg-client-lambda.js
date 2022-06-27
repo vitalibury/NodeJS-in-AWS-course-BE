824813a77c51793e4cdf062fac2db52ee6d35117
Vitaly Bury <vitalybury@coherentsolutions.com>
2022-06-27 10:07:31 +0300

    delete credentials

--- product-service/pg-client-lambda.js
+++ product-service/pg-client-lambda.js
@@ -2,15 +2,4 @@
 import { Client } from 'pg';
 
 const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
-const dbOptions = {
-    host: PG_HOST,
-    port: PG_PORT,
-    database: PG_DATABASE,
-    user: PG_USERNAME,
-    password: PG_PASSWORD,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    connectionTimeoutMillis: 5000
-};
-
+const dbOptions = {};
